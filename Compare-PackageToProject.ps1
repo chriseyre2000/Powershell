9e17dd42746d0652e807ec41aac71c86a8260a58
Christopher Eyre <chriseyre2000@gmail.com>
2014-05-18 15:02:14 +0100

    Added utility to compare pacakges.config with the help paths.

---
+++ Compare-PackageToProject.ps1
@@ -0,0 +1,37 @@
+﻿
+
+
+function Compare-PackageToProject()
+{
+<#
+.Synopsis
+Compare the contents of the packages.config with the help paths in the csproj file to see if there are differences.
+Please note that some differences are to be expected.
+
+.EXAMPLE
+dir -Recurse *.Csproj  | % {Compare-PackageToProject -Verbose $_}
+#>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True,Position=1,ValueFromPipelineByPropertyName=$True)]
+        [string]$FullName
+    )
+
+    Write-Verbose $FullName
+
+    $directory = (Get-ChildItem -Path $FullName).Directory
+
+    [xml]$package = get-content $directory\packages.config
+    $pkgdata = $package.packages.package | select @{Name="Path";Expression = {$_.Id + "." + $_.Version}}
+   
+
+    $filename = $FullName
+
+    write-host $filename
+
+    [xml]$project = get-content $filename
+    $projdata = $project.Project.ItemGroup.Reference.HintPath | select -Unique @{Name="Project";Expression={("$_" -split "\\")[2] }}
+
+    Compare-Object $pkgdata $projdata 
+} 
